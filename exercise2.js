3fd72b40938fe7da0138a47efc63106b9e7b1725
Alfred Inga <alfredingarios@gmail.com>
2020-10-13 15:41:02 +0200

    fase123

--- exercise2.js
+++ exercise2.js
@@ -1,12 +1,14 @@
-function fase12()
+function fase123()
 {
     let Name = prompt("What is your name?").toLowerCase();
     let charName = Name.split('');
     let Vocals = ['a', 'e', 'i', 'o', 'u'];
     let Numbers = ['0','1','2','3','4','5','6','7','8','9'];
     let k = 0;
+    let dictionary = {};
     for(let i = 0; i <= charName.length; i++)
     {
+        // Check if the name is correct, it must not have numbers
         while(k <= charName.length)
         {
             if(Numbers.includes(charName[k]))
@@ -15,7 +17,10 @@ function fase12()
                 return
             }
             k++;
+            // We take this iteration also to create our dictionary and afterthat fill it with values
+            dictionary[charName[k]] = 0;
         }
+
         if(Vocals.includes(charName[i]))
         {
             console.log(charName[i] + " is a VOCAL");
@@ -24,5 +29,7 @@ function fase12()
         {
             console.log(charName[i] + " is a CONSONANT")
         }
+        dictionary[charName[i]] = dictionary[charName[i]] + 1;
     }
+    console.log(dictionary)
 }
